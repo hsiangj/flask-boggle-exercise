f72a350f92416e108c87fc299bd59ad287b9352b
Jessica Hsiang <hsiang.jessica@gmail.com>
2023-01-04 11:28:30 -0800

    Set up route to display boggle board

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from boggle import Boggle
-from flask import Flask
+from flask import Flask, request, redirect, session, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
-## set secret key when session is set up
-# app.config['SECRET_KEY'] = ''
+
+app.config['SECRET_KEY'] = 'bogglesecret'
+
 app.debug = True
 toolbar = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
+
+@app.route('/')
+def display_board():
+  """Display Board"""
+  board = boggle_game.make_board()
+  session['board'] = board
+  return render_template("board.html", board=board)
