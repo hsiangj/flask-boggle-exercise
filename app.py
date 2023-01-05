2794c17c3392880df16e23a5ffbac603b704a0b1
Jessica Hsiang <hsiang.jessica@gmail.com>
2023-01-04 16:19:27 -0800

    Renamed board.html to index.html. Set up AJAX and response display in app.js. Added check_word route in app.py.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from boggle import Boggle
-from flask import Flask, request, redirect, session, render_template
+from flask import Flask, request, redirect, session, jsonify, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -13,8 +13,17 @@ toolbar = DebugToolbarExtension(app)
 boggle_game = Boggle()
 
 @app.route('/')
-def display_board():
+def index():
   """Display Board"""
   board = boggle_game.make_board()
   session['board'] = board
-  return render_template("board.html", board=board)
+  return render_template("index.html", board=board)
+
+@app.route('/check_word')
+def check_word():
+  """Check if word is in dictionary"""
+  word = request.args['guess']
+  board = session['board']
+  word_result = boggle_game.check_valid_word(board, word)
+
+  return jsonify({'result': word_result})
---
+++ static/app.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+$('#submitBtn').on('click', function(e){
+  e.preventDefault();
+  const $inputValue = $('#inputVal').val();
+  getSubmittedWord($inputValue);
+  $('#inputForm').trigger('reset');
+})
+
+async function getSubmittedWord(word){
+  const res = await axios.get('/check_word', {
+    params: {
+      guess: `${word}`
+    }
+  });
+
+  const {result} = res.data;
+  if (result === 'not-word'){
+    $('.msg').text(`${word} is not a valid word`)
+  }else if (result === 'not-on-board'){
+    $('.msg').text(`${word} is not a valid word on this board`)
+  }else {
+    $('.msg').text(`Yay! You found: ${word}`)
+  }
+}
+
+
+
+})
